97e7e651e384532feef1a22b0d052176a4128f0a
stephanie <sminker0401@gmail.com>
2022-09-16 21:15:24 -0400

    saving the final code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
-var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
+var alphaLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 var symbols = "!@#$%^&*_-+=";
 var pw = "";
 
@@ -8,25 +8,49 @@ function generatePassword() {
   var bucket = "";
   var length = prompt("How many characters?");
   console.log(length);
-  var upper = prompt("Do you want upper case letters?");
+  var upper = confirm("Do you want upper case letters?");
   console.log(upper);
-  var lower = prompt("Do you want lower case letters?");
+  var lower = confirm("Do you want lower case letters?");
   console.log(lower);
-  var num = prompt("Would you like numbers?");
+  var num = confirm("Would you like numbers?");
   console.log(num);
-  var sym = prompt("Would you like special characters?");
+  var sym = confirm("Would you like special characters?");
   console.log(sym);
 
 
+var possibleCharacters = [];
+
+if(upper) {
+  possibleCharacters = [...possibleCharacters, ...alphaUpper];
 }
 
+if(lower) {
+  possibleCharacters = [...possibleCharacters, ...alphaLower];
+}
 
-// Get references to the #generate element
+if(num) {
+  possibleCharacters = [...possibleCharacters, ...numbers];
+}
+
+if(sym) {
+  possibleCharacters = [...possibleCharacters, ...symbols];
+}
+
+var generatedPassword = '';
+
+for(i = 0; i < length; i++) {
+  var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+  generatedPassword += possibleCharacters[randomIndex];
+}
+
+return generatedPassword;
+
+}
+
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -35,5 +59,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
